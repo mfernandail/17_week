c4c18188e6fb96945af3c18d32086c65e544c42a
mfernandail <maria.ibanez.leiva@gmail.com>
2025-08-17 17:31:35 -0400

    Refactor variable names for consistency in the Guess the Number game

--- games/guess.js
+++ games/guess.js
@@ -1,8 +1,8 @@
-const guessInput = document.getElementById('guessInput')
-const guessBtn = document.getElementById('guessBtn')
-const message = document.getElementById('message')
-const resetBtn = document.getElementById('resetBtn')
-const attempts = document.getElementById('attempts')
+const $guessInput = document.getElementById('$guessInput')
+const $guessBtn = document.getElementById('$guessBtn')
+const $message = document.getElementById('$message')
+const $resetBtn = document.getElementById('$resetBtn')
+const $attempts = document.getElementById('$attempts')
 
 const MIN = 1
 const MAX = 100
@@ -12,15 +12,15 @@ let COUNTER = 0
 
 //RANDOM_NUMBER = Math.floor(Math.random() * 100) + 1
 
-guessInput.addEventListener('keydown', (e) => {
+$guessInput.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') startGame()
 })
 
-guessBtn.addEventListener('click', startGame)
-resetBtn.addEventListener('click', resetGame)
+$guessBtn.addEventListener('click', startGame)
+$resetBtn.addEventListener('click', resetGame)
 
 function startGame() {
-  const raw = guessInput.value.trim()
+  const raw = $guessInput.value.trim()
   const player_number = Number(raw)
 
   if (
@@ -29,29 +29,29 @@ function startGame() {
     player_number < MIN ||
     player_number > MAX
   ) {
-    message.textContent = `Please enter a valid number between ${MIN} and ${MAX}.`
-    guessInput.focus()
+    $message.textContent = `Please enter a valid number between ${MIN} and ${MAX}.`
+    $guessInput.focus()
     return
   }
 
   if (player_number === RANDOM_NUMBER) {
     COUNTER++
-    attempts.textContent = COUNTER
-    message.textContent = `You win the number is ${player_number}`
-    guessInput.value = null
+    $attempts.textContent = COUNTER
+    $message.textContent = `You win the number is ${player_number}`
+    $guessInput.value = null
     RANDOM_NUMBER = Math.floor(Math.random() * 100) + 1
-    guessInput.focus()
+    $guessInput.focus()
   } else if (player_number > RANDOM_NUMBER) {
-    message.textContent = 'Too high. Try a smaller number.'
+    $message.textContent = 'Too high. Try a smaller number.'
   } else {
-    message.textContent = 'Too low. Try a bigger number.'
+    $message.textContent = 'Too low. Try a bigger number.'
   }
 }
 
 function resetGame() {
-  message.textContent = 'Make a guess!'
-  attempts.textContent = 0
-  guessInput.value = null
+  $message.textContent = 'Make a guess!'
+  $attempts.textContent = 0
+  $guessInput.value = null
 }
 
 function getRandom(min, max) {
